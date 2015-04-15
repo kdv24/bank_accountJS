df093d0b1e4fb1e5c02590a46966c7fd3c9326a2
Abeer Khakwani & kelly de vries <student@epicodus.com>
2015-04-15 12:08:21 -0700

    Add jQuery to make initial deposit add to balance on form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 var BankAccount = {
     balance: 0,
     withdraw: function(amount) {
-        this.balance = this.balance - amount;
+      this.balance = this.balance - amount;
     },
     deposit: function(amount) {
-        this.balance = this.balance + amount;
+      this.balance = this.balance + amount;
     }
 };
 
 $(document).ready(function(){
+  $("form#sign-in").submit(function(event){
+    event.preventDefault();
+    var name=$("input#name").val();
+    var IDeposit=parseInt($("input#InitialDeposit").val());
 
+
+
+    var newAccount = Object.create(BankAccount);
+    newAccount.deposit(IDeposit);
+
+    $("#FinalBalance").text(newAccount.balance);
+
+
+  });
 });
