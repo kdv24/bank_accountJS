2b6908c5c219d458893aebccf91d7123a279ee95
Abeer Khakwani & kelly de vries <student@epicodus.com>
2015-04-15 15:34:28 -0700

    Add color coding based on dollar amount.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,25 @@ var BankAccount = {
     }
 };
 
+var changeColor= function (balance){
+
+  if(balance < 0){
+    $("h2").css("color","red");
+  }
+
+  else if(balance <= 99.00){
+    $("h2").css("color","purple");
+  }
+
+  else if (balance < 500.00){
+    $("h2").css("color","blue");
+  }
+
+  else if (balance >= 500.00){
+    $("h2").css("color","green");
+  }
+}
+
 var newAccount = Object.create(BankAccount);
 
 
@@ -21,6 +40,7 @@ $(document).ready(function(){
     newAccount.personName= name;
 
     $(".Account-holder").text(newAccount.personName);
+    changeColor(newAccount.balance);
     $("#FinalBalance").text(newAccount.balance.toFixed(2));
     $(".hideUntilNewAccount").show();
     $(".hideAfterNewAccountIsMade").css("display","none");
@@ -32,8 +52,33 @@ $(document).ready(function(){
   $("form#deposit").submit(function(event){
     event.preventDefault();
     var newDeposit=parseFloat($("input#DepositAmount").val());
+    var newWithdrawal=parseFloat($("input#WithdrawalAmount").val());
+    if (isNaN(newDeposit) && isNaN(newWithdrawal)){
+      alert("Please enter a number");
+      newDeposit=0;
+      newWithdrawal=0;
+    }
+
+    else if (!newDeposit){
+      newDeposit=0;
+    }
+    else if(!newWithdrawal){
+
+      newWithdrawal=0;
+    }
+
+
+
+
     newAccount.deposit(newDeposit);
+    newAccount.withdraw(newWithdrawal);
+    console.log(newAccount.balance.toFixed(2));
+    changeColor(newAccount.balance);
+
     $("#FinalBalance").text(newAccount.balance.toFixed(2));
+
+    $("input#DepositAmount").val("");
+    $("input#WithdrawalAmount").val("")
   });
 
 });
